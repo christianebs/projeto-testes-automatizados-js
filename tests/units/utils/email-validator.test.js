02b4c4ddbe89f510e43a9596e425523c8e7f835e
Patrick Farias <patrickfarias@live.com>
2024-03-17 16:37:07 -0300

    Create email-validator.test.js

---
+++ tests/units/utils/email-validator.test.js
@@ -0,0 +1,18 @@
+const Email = require('../../../src/utils/email-validator');;
+
+describe('Email Validator tests', () => {
+    test('should return false for empty email', () => {
+        const isValid = Email.isValid('');
+        expect(isValid).toBe(false);
+    });
+
+    test('should return false for invalid email format', () => {
+        const isValid = Email.isValid('invalid-email.com');
+        expect(isValid).toBe(false);
+    });
+
+    test('should return true for valid email format', () => {
+        const isValid = Email.isValid('valid.email@example.com');
+        expect(isValid).toBe(true);
+    });
+});
