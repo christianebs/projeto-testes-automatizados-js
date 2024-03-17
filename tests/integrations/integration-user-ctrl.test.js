d1d987842badfe9abc5d569a113c0ccaefaef0f1
Patrick Farias <patrickfarias@live.com>
2024-03-17 16:21:36 -0300

    Create integration-user-ctrl.test.js

---
+++ tests/integrations/integration-user-ctrl.test.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const UserController = require('../../src/controllers/user-ctrl');
+const { res, req } = require('../mocks/test-mocks');
+
+describe('[integration][session-ctrl] - create', () => {
+  beforeAll(async () => {
+    await mongoose.connect(process.env.MONGO_DB_BASE_URL);
+  });
+
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  test('Should return status 201 for a new user created at database', async () => {
+    jest.spyOn(res, 'status');
+    await UserController.create(req('success'), res);
+    expect(res.status).toBeCalledWith(201);
+  });
+  
+    test('Should return status 400 if the email is invalid', async () => {
+      jest.spyOn(res, 'status');
+      await UserController.create(req('no-email'), res);
+      expect(res.status).toBeCalledWith(400);
+    });
+
+  test('Should return status 400 if the password is not provided', async () => {
+    jest.spyOn(res, 'status');
+    await UserController.create(req('no-password'), res);
+    expect(res.status).toBeCalledWith(400);
+  });
+});
